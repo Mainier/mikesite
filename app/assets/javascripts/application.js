e15fbe3d90ca2752feda8ef2224c0bec292d6a07
Mainier <michaelmainier@Michaels-MacBook-Pro.local>
2013-12-13 12:54:26 -0500

    Created application.js file, added javascript_include_tag to application.hgml.erb, created asset pipeline within application.js, this fixed scrolling issue and map issues.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery-1.7.2.min
+//= require bootstrap.min
+//= require modernizr
+//= require jquery.ui.map.full.min
+//= require jquery.scrollTo-1.4.3.1-min
+//= require jquery.localscroll-1.2.7-min
+//= require main
+
